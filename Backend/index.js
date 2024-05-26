bf824a3a2f7fb497477fbea04793bc0b585e635e
Irfan <irfanyusufk13@gmail.com>
2024-05-27 01:25:57 +0700

    backend done - middleware

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./src/config/db.config');
 const expenseController = require('./src/controllers/expenseController');
+const accountController = require('./src/controllers/accountControllers');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,6 +23,11 @@ app.get('/getExpensesByCategory/:category', expenseController.getExpensesByCateg
 app.get('/getTotalExpense', expenseController.getTotalExpense);
 app.put('/updateExpense/:id', expenseController.updateExpense);
 app.delete('/deleteExpense/:id', expenseController.deleteExpense);
+
+// Account operations
+app.post('/register', accountController.register);
+app.post('/login', accountController.login);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
